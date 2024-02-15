fixed single cell masking exploding; numerous other fixes

cal_track.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 import os
 from matplotlib import colormaps
@@ -5,10 +6,20 @@ from matplotlib import pyplot as plt
 from scipy.io import loadmat
 import numpy.typing as npt
 
-from core.analysis import beat_segmentation, get_calcium_trace, get_parameters
-from core.masking import get_mask, get_mask_multi_cell, get_mask_multi_cell_v2
-from core.reader import get_video_frames
-from parameters import path_to_single_cell_video_directory
+from core.analysis import (
+    beat_segmentation,
+    get_calcium_trace,
+    get_parameters,
+    photo_bleach_correction,
+)
+from core.masking import (
+    get_mask_single_cell,
+    get_mask_multi_cell,
+    get_mask_multi_cell_v2,
+)
+from core.reader import get_video_frames, post_read, pre_read
+import single_cell_parameters
+import multi_cell_parameters
 
 PATH_ND2_TEST = (
     "/home/mkurnia/uni/fyp/PyCalTrack/sample_videos/"
@@ -20,45 +31,153 @@ PATH_VSI_TEST = (
 PATH_MULTICELL_TIF_TEST = "/home/mkurnia/uni/fyp/PyCalTrack/sample_videos/video1.tif"
 
 
-def _get_videos(paths: list[str]) -> list[npt.NDArray]:
+def _get_paths(path_to_directory: str) -> list[str]:
+    if not path_to_directory:
+        return []
+    try:
+        paths = []
+        for path in os.scandir(path_to_directory):
+            if path.is_file():
+                paths.append(path.path)
+        return paths
+    except FileNotFoundError:
+        print(f"Error: {single_cell_parameters.videos_directory} does not exist.")
+        return []
+
+
+def _get_videos(paths: list[str]) -> list[Tuple[npt.NDArray, str]]:
     result = []
     for path in paths:
         video_frames = get_video_frames(path)
         if video_frames is not None:
-            result.append(video_frames)
+            result.append((video_frames, path))
     return result
 
 
 def main() -> None:
-    for file_name in os.scandir(path_to_single_cell_video_directory):
-        if file_name.is_file():
-            print(file_name.path)
+    single_cell_video_paths = _get_paths(single_cell_parameters.videos_directory)
+    multi_cell_video_paths = _get_paths(multi_cell_parameters.videos_directory)
 
-    return
-    # frames = get_video_frames(PATH_MULTICELL_TIF_TEST)
-    # if frames is None:
-    #     print("AAAAAAAAAAAAAAAAAAAA")
-    #     return
-    # f, axarr = plt.subplots(1, 2)
-    # # axarr[0, 0].imshow(image_datas[0])
-    # # axarr[0, 1].imshow(image_datas[1])
-    # mask = get_mask_multi_cell(frames)
-    # my_mask = mask / 255
-    # axarr[0].imshow(mask, cmap=colormaps["gray"])
-    # mask = loadmat(
-    #     "/home/mkurnia/uni/fyp/PyCalTrack/sample_videos/benchmark/multi_cell/ProjFgMask.mat"
-    # )["ProjFgMask"]
-    # axarr[1].imshow(mask, cmap=colormaps["gray"])
-    # print(np.max(my_mask - mask))
-    # # print(np.max(np.mean(frames, axis=0) - np.mean(mask / 65535, axis=2)))
+    if len(single_cell_video_paths) == 0 and len(multi_cell_video_paths) == 0:
+        return
+
+    pre_read()
+    single_cell_traces = []
+    n_success = 0
+
+    single_cell_videos = _get_videos(single_cell_video_paths)
+    for video_frames, path in single_cell_videos:
+        mask = get_mask_single_cell(video_frames)
+        calcium_trace = get_calcium_trace(video_frames, mask)
+        ignored_trace = calcium_trace[: single_cell_parameters.beginning_frames_removed]
+        analysed_trace = calcium_trace[
+            single_cell_parameters.beginning_frames_removed :
+        ]
+        beat_segments = beat_segmentation(
+            analysed_trace,
+            single_cell_parameters.acquisition_frequency,
+            single_cell_parameters.pacing_frequency,
+            single_cell_parameters.max_pacing_deviation,
+        )
+        if beat_segments is not None:
+            corrected_trace = photo_bleach_correction(
+                analysed_trace,
+                beat_segments,
+                single_cell_parameters.acquisition_frequency,
+                single_cell_parameters.pacing_frequency,
+            )
+            n_success += 1
+        else:
+            corrected_trace = None
+
+        single_cell_traces.append(
+            (
+                path[len(single_cell_parameters.videos_directory) + 1 :],
+                analysed_trace,
+                corrected_trace,
+                beat_segments is not None,
+            )
+        )
+
+    # n_rows = max(len(single_cell_videos) - n_success, n_success)
+    # plt.subplot2grid((n_rows, 3), (0, 0)).figtext(0.25, 0.8, "Original Trace(s)")
+    # plt.subplot2grid((n_rows, 3), (0, 1)).figtext(0.5, 0.8, "Corrected Trace(s)")
+    # plt.subplot2grid((n_rows, 3), (0, 2)).figtext(0.75, 0.8, "Ignored Trace(s)")
+    # i_successful = 1
+    # i_failed = 1
+    # for original_trace, corrected_trace, successful in single_cell_traces:
+    #     if successful:
+    #         plt.subplot2grid((n_rows, 3), (i_successful, 0)).plot(original_trace)
+    #         plt.subplot2grid((n_rows, 3), (i_successful, 1)).plot(corrected_trace)
+    #         i_successful += 1
+    #     else:
+    #         plt.subplot2grid((n_rows, 3), (i_failed, 2)).plot(original_trace)
+    #         i_failed += 1
     # plt.show()
 
-    # """
+    fig, big_axes = plt.subplots(nrows=1, ncols=3)
+    fig.tight_layout()
+    big_axes[0].set_title("Original Trace(s)", fontsize=16, pad=48)
+    big_axes[1].set_title("Corrected Trace(s)", fontsize=16, pad=48)
+    big_axes[2].set_title("Ignored Trace(s)", fontsize=16, pad=48)
+
+    for big_ax in big_axes:
+        big_ax.axis("off")
+
+    n_rows = max(len(single_cell_videos) - n_success, n_success)
+    i_successful = 0
+    i_failed = 0
+    for name, original_trace, corrected_trace, successful in single_cell_traces:
+        if successful:
+            ax = fig.add_subplot(n_rows, 3, i_successful * 3 + 1)
+            ax.plot(original_trace)
+            ax.set_title(name)
+            ax = fig.add_subplot(n_rows, 3, i_successful * 3 + 2)
+            ax.plot(corrected_trace)
+            ax.set_title(name)
+            i_successful += 1
+        else:
+            ax = fig.add_subplot(n_rows, 3, i_failed * 3 + 3)
+            ax.plot(original_trace)
+            ax.set_title(name)
+            i_failed += 1
+
+    plt.tight_layout(pad=0.6)
+    plt.show()
+
+    multi_cell_videos = _get_videos(multi_cell_video_paths)
+    post_read()
+
+    return
+
+    pre_read()
+    frames = get_video_frames(
+        "/home/mkurnia/uni/fyp/PyCalTrack/sample_videos/notworkinglol/Process_2888.vsi"
+    )
+    post_read()
+    if frames is None:
+        print("AAAAAAAAAAAAAAAAAAAA")
+        return
+    f, axarr = plt.subplots(1, 2)
+    # axarr[0, 0].imshow(image_datas[0])
+    # axarr[0, 1].imshow(image_datas[1])
+    mask = get_mask_single_cell(frames)
+    my_mask = mask / 255
+    axarr[0].imshow(mask, cmap=colormaps["gray"])
+    mask = loadmat("/home/mkurnia/uni/fyp/PyCalTrack/sample_videos/FinalMask.mat")[
+        "FinalMask"
+    ]
+    axarr[1].imshow(mask, cmap=colormaps["gray"])
+    # print(np.max(my_mask - mask))
+    # print(np.max(np.mean(frames, axis=0) - np.mean(mask / 65535, axis=2)))
+    plt.show()
+
+    """
     frames = get_video_frames(PATH_VSI_TEST)
     if frames is None:
         return
 
-    mask = get_mask(frames)
+    mask = get_mask_single_cell(frames)
     # plt.imshow(mask, cmap=colormaps["gray"])
     # plt.show()
 
@@ -86,7 +205,7 @@ def main() -> None:
     left, right = beat_segments[0]
 
     get_parameters(calcium_trace[left:right], 50, 1)
-    # """
+    """
 
 
 if __name__ == "__main__":

core/analysis.py
@@ -24,6 +24,8 @@ def get_calcium_trace(frames: npt.NDArray, mask: npt.NDArray) -> npt.NDArray:
 def _find_peak_indices(x: npt.NDArray) -> npt.NDArray:
     peaks_raw, _ = find_peaks(x)
     prominences, *_ = peak_prominences(x, peaks_raw)
+    if prominences.size == 0:
+        return np.array([])
     indices, _ = find_peaks(
         x, prominence=PEAK_PROMINENCE_THRESHOLD * np.max(prominences)
     )
@@ -35,7 +37,6 @@ def beat_segmentation(
     acquisition_frequency: float,
     pacing_frequency: float,
     max_pacing_deviation: float,
-    single_beat: bool = False,
 ) -> Union[list[Tuple[int, int]], None]:
     # Handle acquisition frequency > 100.
     calcium_trace_original = calcium_trace
@@ -49,7 +50,8 @@ def beat_segmentation(
         stride = 1
 
     pacing_period = 1 / pacing_frequency
-    cutoff_threshold = pacing_period * (1 - max_pacing_deviation)
+    cutoff_lower_bound = pacing_period * (1 - max_pacing_deviation)
+    cutoff_upper_bound = pacing_period * (1 + max_pacing_deviation)
 
     # Offset is 10% of the pacing period in number of frames.
     offset = ceil(TRACE_ONSET_DELAY * pacing_period * acquisition_frequency)
@@ -68,15 +70,22 @@ def beat_segmentation(
 
     trace_peak_periods = np.diff(trace_peak_indices / acquisition_frequency)
     # Check if there are any periods which is less than our cutoff threshold.
-    extra_beat_detected = np.any(trace_peak_periods < cutoff_threshold)
+    deviation_detected = np.any(trace_peak_periods < cutoff_lower_bound)
+    deviation_detected |= np.any(trace_peak_periods > cutoff_upper_bound)
 
     if (diff_peak_indices.size - trace_peak_indices.size) > 1:
         # Number of peaks in gradient does not match with number of peaks in value.
-        # TODO: Return early here.
+        print(
+            "Number of peaks in gradient does not match with number of peaks in value, skipping."
+        )
         return None
-    elif extra_beat_detected:
-        # Period between beats less than cutoff threshold.
-        # TODO: Return early here.
+    elif deviation_detected:
+        # Beat periods deviate too much from specified pacing period.
+        print("Beat periods deviate too much from specified pacing period, skipping.")
+        return None
+    elif diff_peak_indices.size == 0:
+        # Trace too flat, unable to detect any peaks in gradient.
+        print("Flat trace detected, skipping.")
         return None
 
     segmented_beats: list[npt.NDArray] = []
@@ -110,7 +119,7 @@ def beat_segmentation(
         last_transient_period = (
             last_transient_end - last_transient_start
         ) / acquisition_frequency
-        if last_transient_period >= cutoff_threshold:
+        if last_transient_period >= cutoff_lower_bound:
             start = last_transient_start * stride
             end = last_transient_end * stride
             segmented_beats.append(calcium_trace_original[start:end])
@@ -149,7 +158,7 @@ def photo_bleach_correction(
     beat_segments: list[Tuple[int, int]],
     acquisition_frequency: float,
     pacing_frequency: float,
-) -> None:
+) -> npt.NDArray:
     # TODO: Rename variables to something more sensible.
     pacing_period = 1 / pacing_frequency
     baseline_duration = ceil(BASELINE_WINDOW * pacing_period * acquisition_frequency)
@@ -164,12 +173,12 @@ def photo_bleach_correction(
         magnitude = peak - baseline
         val95 = baseline + 0.05 * magnitude
         baseline_values_mask = segmented_beat < val95
-        np.append(xs, segmented_beat[baseline_values_mask])
-        np.append(ys, np.arange(start, end)[baseline_values_mask])
+        xs = np.append(xs, np.arange(start, end)[baseline_values_mask])
+        ys = np.append(ys, segmented_beat[baseline_values_mask])
 
     # we are trying to fit a polynomial of degree 2
     polynomial = lambda x, a, b, c: a * x * x + b * x + c
-    parameters, *_ = curve_fit(polynomial, xs, ys)
+    parameters, *_ = curve_fit(polynomial, xs, ys, p0=[1, 1, baseline])
     trend = np.polyval(parameters, np.arange(calcium_trace.size))
 
     corrected_trace = calcium_trace - trend + trend[0]

core/masking.py
@@ -5,28 +5,30 @@ import numpy as np
 import numpy.typing as npt
 
 
-def get_mask(frames: npt.NDArray) -> npt.NDArray:
+def get_mask_single_cell(frames: npt.NDArray) -> npt.NDArray:
     # Assuming that the each pixel in every frame have range [0, 1].
-    frames = (255 * frames).astype(np.uint8)
+    frames = (65535 * frames).astype(np.uint16)
 
     height, width, n_frames = frames.shape
 
     stacked_frames = frames.reshape(height * n_frames, width)
 
     threshold, _ = cv2.threshold(
-        stacked_frames, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+        stacked_frames, 0, 65535, cv2.THRESH_BINARY + cv2.THRESH_OTSU
     )
 
     raw_masks = np.zeros_like(frames)
     for i, frame in enumerate(frames):
-        _, raw_threshold = cv2.threshold(frame, 0, threshold, cv2.THRESH_BINARY)
+        _, raw_threshold = cv2.threshold(frame, threshold, 65535, cv2.THRESH_BINARY)
 
         raw_masks[i] = raw_threshold
 
-    mean_raw_mask = (np.mean(raw_masks, axis=0) * 255).astype(np.uint8)
+    mean_raw_mask = np.clip(np.mean(raw_masks, axis=0), 0, 65535).astype(np.uint16)
     _, mean_raw_mask_binarized = cv2.threshold(
-        mean_raw_mask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+        mean_raw_mask, 0, 65535, cv2.THRESH_BINARY + cv2.THRESH_OTSU
     )
+
+    mean_raw_mask_binarized = mean_raw_mask_binarized.astype(np.uint8)
 
     (
         n_blobs,
@@ -41,16 +43,17 @@ def get_mask(frames: npt.NDArray) -> npt.NDArray:
 
     min_size = 500
 
-    resulting_mask = np.zeros_like(image_with_separated_blobs, dtype=np.uint8)
+    resulting_mask = np.zeros_like(image_with_separated_blobs, dtype=np.uint16)
 
     for blob in range(n_blobs):
         if sizes[blob] >= min_size:
-            resulting_mask[image_with_separated_blobs == blob + 1] = 255
+            resulting_mask[image_with_separated_blobs == blob + 1] = 65535
 
     dilation_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
     resulting_mask = cv2.dilate(resulting_mask, dilation_kernel)
 
     resulting_mask = cv2.bitwise_not(resulting_mask)
+    resulting_mask = resulting_mask.astype(np.uint8)
 
     (
         n_blobs,
@@ -73,11 +76,6 @@ def get_mask(frames: npt.NDArray) -> npt.NDArray:
 
     resulting_mask = resulting_mask.astype(np.bool_)
 
-    asdf = mean_raw_mask
-    asdf[np.invert(resulting_mask)] = (
-        0.5 * mean_raw_mask[np.invert(resulting_mask)] + 120
-    )
-
     return resulting_mask
 
 
@@ -95,7 +93,7 @@ def get_mask_multi_cell(frames: npt.NDArray) -> npt.NDArray:
     average_frame_rescaled = np.interp(
         average_frame, (average_frame.min(), average_frame.max()), (0, 1)
     )
-    average_frame_rescaled = (255 * average_frame_rescaled).astype(np.uint8)
+    average_frame_rescaled = (65535 * average_frame_rescaled).astype(np.uint16)
 
     # Here we follow matlab's cliplimit.
     # TODO: Test that this indeed gives us the same output.
@@ -106,11 +104,11 @@ def get_mask_multi_cell(frames: npt.NDArray) -> npt.NDArray:
     gauss_1 = cv2.GaussianBlur(average_frame_equalised, _get_kernel_size(2.5), 2.5, 2.5)
     gauss_2 = cv2.GaussianBlur(average_frame_equalised, _get_kernel_size(0.2), 0.2, 0.2)
     diff_gauss = np.clip(
-        gauss_1.astype(np.int16) - gauss_2.astype(np.int16), 0, 255
-    ).astype(np.uint8)
+        gauss_1.astype(np.int16) - gauss_2.astype(np.int16), 0, 65535
+    ).astype(np.uint16)
     diff_gauss_equalised = clahe.apply(clahe.apply(diff_gauss))
     _, cell_partition_negative = cv2.threshold(
-        diff_gauss_equalised, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+        diff_gauss_equalised, 0, 65535, cv2.THRESH_BINARY + cv2.THRESH_OTSU
     )
     cell_partition = cv2.bitwise_not(cell_partition_negative)
     average_frame_partitioned = clahe.apply(
@@ -118,7 +116,7 @@ def get_mask_multi_cell(frames: npt.NDArray) -> npt.NDArray:
     )
 
     _, raw_mask = cv2.threshold(
-        average_frame_partitioned, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+        average_frame_partitioned, 0, 65535, cv2.THRESH_BINARY + cv2.THRESH_OTSU
     )
 
     erosion_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
@@ -169,7 +167,7 @@ def get_mask_multi_cell_v2(frames: npt.NDArray) -> npt.NDArray:
     # Assuming that the each pixel in every frame have range [0, 1].
     average_frame = np.mean(frames, axis=0)
     I = np.interp(average_frame, (average_frame.min(), average_frame.max()), (0, 1))
-    I = (255 * I).astype(np.uint8)
+    I = (65535 * I).astype(np.uint16)
 
     # Here we follow matlab's cliplimit.
     # TODO: Test that this indeed gives us the same output.
@@ -181,19 +179,19 @@ def get_mask_multi_cell_v2(frames: npt.NDArray) -> npt.NDArray:
     gauss_2 = cv2.GaussianBlur(asdf, _get_kernel_size(0.2), 0.2, 0.2)
     diff_gauss = gauss_1.astype(np.int16) - gauss_2.astype(np.int16)
     diff_gauss = np.clip(
-        gauss_1.astype(np.int16) - gauss_2.astype(np.int16), 0, 255
-    ).astype(np.uint8)
+        gauss_1.astype(np.int16) - gauss_2.astype(np.int16), 0, 65535
+    ).astype(np.uint16)
 
     clahe = cv2.createCLAHE(clipLimit=40)  # keep note of this in the notes
     diff_gauss = clahe.apply(diff_gauss)
     # diff_gauss = clahe.apply(diff_gauss)
 
     _, cell_partition_negative = cv2.threshold(
-        diff_gauss, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+        diff_gauss, 0, 65535, cv2.THRESH_BINARY + cv2.THRESH_OTSU
     )
     cell_partition = cv2.bitwise_not(cell_partition_negative)
 
-    _, mask = cv2.threshold(asdf, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, mask = cv2.threshold(asdf, 0, 65535, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     mask = mask * cell_partition
 
     SE = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))

core/reader.py
@@ -10,13 +10,20 @@ import numpy as np
 import numpy.typing as npt
 
 
+def pre_read() -> None:
+    javabridge.start_vm(class_path=bf.JARS)
+
+
+def post_read() -> None:
+    javabridge.kill_vm()
+
+
 def _get_video_frames_nd2(path: str, nd2_calcium_layer_index=0) -> npt.NDArray:
     images = nd2.imread(path)
     return images[:, nd2_calcium_layer_index, :, :]
 
 
 def _get_video_frames_vsi(path: str) -> Union[npt.NDArray, None]:
-    javabridge.start_vm(class_path=bf.JARS)
     t = 0
     result = []
     try:
@@ -27,7 +34,6 @@ def _get_video_frames_vsi(path: str) -> Union[npt.NDArray, None]:
                 t += 1
     except javabridge.JavaException:
         print(f"Java exception at t = {t}. Assuming EOF.")
-    javabridge.kill_vm()
     return np.stack(result, axis=0)
 
 
@@ -43,7 +49,7 @@ def _get_video_frames_multipage(path: str) -> npt.NDArray:
     return frames
 
 
-def _get_video_frames_other(path: str) -> npt.NDArray:
+def _get_video_frames_other(path: str) -> Union[npt.NDArray, None]:
     capture = cv2.VideoCapture(path)
     n_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
     frames = []
@@ -62,6 +68,8 @@ def _get_video_frames_other(path: str) -> npt.NDArray:
 
         frames.append(image)
 
+    if len(frames) == 0:
+        return None
     return np.array(frames)
 
 
@@ -98,12 +106,9 @@ def get_video_frames(path: str, nd2_calcium_layer_index=0) -> Union[npt.NDArray,
             case _:
                 result = _get_video_frames_other(path)
     except FileNotFoundError:
-        print(f"File with path {path} cannot be found. Cancelling.")
-        if format == ".vsi":
-            javabridge.kill_vm()
-    # except:
-    #     print("An unexpected exception occurred whilst reading files. Cancelling.")
-    #     if format == ".vsi":
-    #         javabridge.kill_vm()
+        print(f"File with path {path} cannot be found, ignoring...")
+    except:
+
+        print(f"An unexpected exception occurred whilst reading {path}, ignoring...")
 
     return result

core/utils.py
@@ -1,13 +1,16 @@
+from math import floor
 import numpy as np
 import numpy.typing as npt
 
 
 def moving_average(x: npt.NDArray, k: int) -> npt.NDArray:
     middle = np.convolve(
-        np.diff(x),
+        x,
         np.ones(k) / k,
         mode="valid",
     )
-    front = np.array([np.mean(x[: i + 1]) for i in range(k - 1)])
-    back = np.array([np.mean(x[-i - 1 :]) for i in range(k - 1)])
+    front = np.array([np.mean(x[: i + 1]) for i in range(floor(k / 2))])
+    back = np.array(
+        [np.mean(x[-i - 1 :]) for i in range(floor(k / 2) - (1 if k % 2 == 0 else 0))]
+    )
     return np.concatenate((front, middle, back), axis=0)